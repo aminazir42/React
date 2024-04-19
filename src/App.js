27ad73bd0dfb5fe78a0feb6d4bc0d3042d71aae7
Amina Nazir <aminazir23@gmail.com>
2024-04-19 15:12:39 +0500

    comiit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import './App.css';
+import NavbarComponent from './Navbar';
 
-function NavigationBar() {
+function App() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React Navbar</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#services">Services</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className="App">
+      <NavbarComponent />
+      {/* Other content of your application */}
+    </div>
   );
 }
 
-export default NavigationBar;
+export default App;
