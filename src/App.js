d97a606c9ece107159c31bfe3b4bb86d3ad6f3d4
Amina Nazir <aminazir23@gmail.com>
2024-04-19 14:49:46 +0500

    jjj

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
-function BrandExample() {
+function NavigationBar() {
   return (
-    <>
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand>Brand text</Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              src="/img/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/img/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React Navbar</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#services">Services</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-export default BrandExample;
+export default NavigationBar;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavigationBar from './components/NavigationBar';
+
+function App() {
+  return (
+    <div>
+      <NavigationBar />
+      {/* Other content of your application */}
+    </div>
+  );
+}
+
+export default App;
